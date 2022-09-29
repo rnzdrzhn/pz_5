2e303857128f656eeced4348864ae4c59c53dc85
Арина Задорожняя <zadoroozhnyayaa@gmail.com>
2022-09-29 11:03:06 +0500

    Добавила 6 практику

---
+++ pz_6/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину отрезка А");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину отрезка В");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int d = b;
+
+            while (a - b > 0)
+            {
+                b = b + d;
+            }
+
+            b = b - d;
+            Console.WriteLine("Длина незанятой части отрезка А = " + (a - b));
+        }
+    }
+}
